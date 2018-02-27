ecf7dba7ed5abedace96ff737b8c3b9992622f63
fdoperezr <fdoperezr@outlook.com>
2018-02-27 13:04:13 -0300

    Final

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -29,12 +29,11 @@
 </html>
 	<script type="text/javascript">
 	function mostrarLista(){
-		var lista = document.getElementById('lista');
-		
-		var numero = document.getElementById('numero').value;
+		var lista = document.getElementById('lista'); //Referencia a div lista
+		var numero = document.getElementById('numero').value; //Referencia a número de entradas
 		lista.innerHTML = "<h4>Lista de Entradas</h4>";
 		if (numero > 0){
-			lista.innerHTML = "<h4>Lista de Entradas</h4>";
+			lista.innerHTML = "<h4>Lista de Entradas</h4>"; //Se crea dinamicamente la lista de entradas
 			for (i = 0; i < numero; i++) { 
 				lista.innerHTML += "Fecha "+(i+1)+":</h6> <input type='date' name='fechas' onchange='compararFechas();'> Número "+(i+1)+": <input type='number' min='1' name='numeros'><br>";
 			}
@@ -46,28 +45,26 @@
 	}
 
 	function calcularFechas(){
-		var resultado = document.getElementById("resultados");
+		var resultado = document.getElementById("resultados"); //Referencia a div para mostrar resultados
 		ocultarMensaje (resultado);
 		if(validarInputs())
 			if (!compararFechas()){
-				console.log("hola");
-				var fechas = document.getElementsByName("fechas");
-				var numeros = document.getElementsByName("numeros");
+				var fechas = document.getElementsByName("fechas"); //Referencia a las entradas tipo date
+				var numeros = document.getElementsByName("numeros"); //Referencia a las entradas tipo number
 				
-				for (i = 0; i < fechas.length; i++){
-					var fecha = new Date (fechas[i].value);
-					var n  = Number(numeros[i].value);
-					var fechaProxima = new Date (fecha);
-					var contador = 0;
-					while (contador <= n ){
-						fechaProxima.setDate(fechaProxima.getDate() +1);
-						if (fechaProxima.getDay() !=0)
-							if (fechaProxima.getDay() != 6){
-								contador++;
-						}
-							
+				for (i = 0; i < fechas.length; i++){ //Por cada entrada
+					var fecha = new Date (fechas[i].value); //Se crea un objeto Date
+					var n  = Number(numeros[i].value); //Se extrae el valor númerico de la entrada number
+					var fechaProxima = new Date (fecha); //Fecha auxiliar
+					var contador = n;
+					fechaProxima.setDate(fechaProxima.getDate() +1);
+					while (contador != 0 ){	
+						fechaProxima.setDate(fechaProxima.getDate() +1); //Se aumenta un dia a la fecha						
+						if (fechaProxima.getDay() !=6 && fechaProxima.getDay() != 0){ //Evalúa si NO es sabado o domingo
+								contador--;
+						}						
 					}
-					var dd = fechaProxima.getDate();
+					var dd = fechaProxima.getDate();			//Comienza el formateo de la fecha dd/mm/yyyy
 					var mm = fechaProxima.getMonth() + 1;
 					var y = fechaProxima.getFullYear();
 					var formato = dd + "-" + mm + "-" + y;
@@ -78,14 +75,14 @@
 	}
 
 	function validarInputs(){
-		var fechas = document.getElementsByName("fechas");
-		var numeros = document.getElementsByName("numeros");
-		var error = document.getElementById('errorCamposVacios');
-		var resultado = document.getElementById("resultados");
+		var fechas = document.getElementsByName("fechas"); //Referencia a las entradas tipo date
+		var numeros = document.getElementsByName("numeros"); //Referencia a las entradas tipo number
+		var error = document.getElementById('errorCamposVacios'); //Referencia a div para mostrar errores
+		var resultado = document.getElementById("resultados"); //Referencia a div para mostrar resultados
 		ocultarMensaje (resultado);
 		ocultarMensaje(error);
 		for (i = 0; i < fechas.length; i++) {
-		 if (fechas[i].value == "" || numeros[i].value == ""){
+		 if (fechas[i].value == "" || numeros[i].value == ""){ //Evaluá si existen campos vacíos de la lista.
 				error.innerHTML += "<strong>¡Error!</strong> No debe dejar campos vacíos.";
 				error.style.display = "block";
 				return false;
@@ -103,18 +100,18 @@
 	}
 
 	function compararFechas (){
-		var fechas = document.getElementsByName("fechas");
-		var error = document.getElementById('errorComparacion');
-		var resultado = document.getElementById("resultados");
+		var fechas = document.getElementsByName("fechas"); //Referencia a las entradas tipo date
+		var error = document.getElementById('errorComparacion'); //Referencia a las entradas tipo number
+		var resultado = document.getElementById("resultados"); //Referencia a div para mostrar resultados
 		ocultarMensaje (resultado);
 		ocultarMensaje(error);
 		var contador = 0;
 		if (fechas.length > 1){
 			for (i = 0; i < fechas.length-1; i++) {
 				if (fechas[i].value != "" && fechas[i+1].value != ""){
-					var fechax = new Date(fechas[i].value);
+					var fechax = new Date(fechas[i].value); //Crea objetos Date de dos fechas consecutivas de la lista de entradas.
 					var fechay = new Date(fechas[i+1].value);
-					if (fechax > fechay){
+					if (fechax > fechay){ //Evalúa si fecha i es mayor a la fecha i+1
 						error.innerHTML += "Fecha "+(i+1)+" es mayor que Fecha "+(i+2)+".<br>";
 						error.style.display = "block";
 						contador++;
@@ -123,8 +120,6 @@
 					
 			}
 		}
-		console.log("c: "+contador);
-		console.log("f: "+fechas.length);
 		if (contador == 0)
 			return false;
 		else 
